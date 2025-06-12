19a1c632c4bd4a52a5435312903553b286d32c4a
cuongtuooo <cuonganh1004cva@gmail.com>
2025-06-12 13:40:33 +0700

    c

--- src/components/common/ImageGalleryWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import type { ReactImageGalleryProps } from "react-image-gallery";
-
-// Nhập kiểu mặc định và ép lại đúng định dạng JSX hợp lệ
-const ImageGallery = require("react-image-gallery").default as FC<ReactImageGalleryProps>;
-
-export default ImageGallery;
